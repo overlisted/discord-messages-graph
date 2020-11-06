f45d1cc2b16d39af947c63fec28779f2fff546b8
Ilya Maximov <mail@overlisted.net>
2020-11-06 18:07:23 +0100

    make the time unit changeable

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 
+const enumTimeUnit = {
+  "minutes": 60000,
+  "hours": 3600000
+}
+
+const timeUnit = process.argv[2];
+if(!enumTimeUnit[timeUnit]) {
+  console.error("Invalid time unit");
+  process.exit();
+}
+
 const json = JSON.parse(fs.readFileSync("messages.json"));
 
 const data = [];
@@ -8,19 +19,19 @@ const firstTime = new Date(json[json.length - 1].timestamp).getTime();
 const lastTime = new Date(json[0].timestamp).getTime();
 
 // prepare the data array
-for(let i = firstTime; i < lastTime; i += 60000) data.push(0);
+for(let i = firstTime; i < lastTime; i += enumTimeUnit[timeUnit]) data.push(0);
 
 // calculate actual data
 json.forEach(it => {
   const time = firstTime - new Date(it.timestamp).getTime();
-  const minute = ((time % 60000) - time) / 60000;
+  const minute = ((time % enumTimeUnit[timeUnit]) - time) / enumTimeUnit[timeUnit];
   
   data[minute]++;
 });
 
 const result = {
   unit: "messages",
-  timeUnit: "minutes",
+  timeUnit: timeUnit,
   data: data
 };
 
