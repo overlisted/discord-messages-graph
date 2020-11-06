b16b9f2bff37744e2d6b2fd68e9d876bc3943b1f
Ilya Maximov <mail@overlisted.net>
2020-11-06 17:51:12 +0100

    use a better data format

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ const fs = require("fs");
 
 const json = JSON.parse(fs.readFileSync("messages.json"));
 
-const result = [];
+const data = [];
 
 const firstTime = new Date(json[json.length - 1].timestamp).getTime();
 const lastTime = new Date(json[0].timestamp).getTime();
 
-// prepare the results array
-for(let i = firstTime; i < lastTime; i += 60000) result.push(0);
+// prepare the data array
+for(let i = firstTime; i < lastTime; i += 60000) data.push(0);
 
-// calculate actual results data
+// calculate actual data
 json.forEach(it => {
   const time = firstTime - new Date(it.timestamp).getTime();
   const minute = ((time % 60000) - time) / 60000;
   
-  result[minute]++;
+  data[minute]++;
 });
+
+const result = {
+  unit: "messages",
+  timeUnit: "minutes",
+  data: data
+};
 
 fs.writeFileSync("graph-data.json", JSON.stringify(result, null, 2));
